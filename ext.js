cca1e22d713efaf31238632fce35dfd553c4aa96
Maxim <madear0000@bk.ru>
2024-12-04 11:43:29 +0300

    change ti OOP

--- ext.js
+++ ext.js
@@ -2,7 +2,14 @@ Ext.application({
     name: 'AuthApp',
 
     launch: function () {
-        const randomNames = [
+        const app = new AuthApp();
+        app.init();
+    }
+});
+
+class AuthApp {
+    constructor() {
+        this.randomNames = [
             'Иван Иванов',
             'Петр Петров',
             'Сергей Сергеев',
@@ -14,131 +21,166 @@ Ext.application({
             'Анастасия Анастасиева',
             'Мария Мариевна'
         ];
+        this.dataStore = this.createDataStore();
+        this.authWindow = this.createAuthWindow();
+    }
 
-        const createFakeData = () => {
-            return Array.from({
-                length: 10
-            }, () => ({
-                name: randomNames[Math.floor(Math.random() * randomNames.length)],
-                checked: false
-            }));
-        };
+    init() {
+        this.authWindow.show();
+    }
 
-        const dataStore = Ext.create('Ext.data.Store', {
+    createFakeData() {
+        return Array.from({ length: 10 }, () => ({
+            name: this.randomNames[Math.floor(Math.random() * this.randomNames.length)],
+            checked: false
+        }));
+    }
+
+    createDataStore() {
+        return Ext.create('Ext.data.Store', {
             fields: ['name', 'checked'],
-            data: createFakeData()
+            data: this.createFakeData()
         });
+    }
 
-        const authForm = Ext.create('Ext.form.Panel', {
-            width: 300,
-            bodyPadding: 10,
-            items: [{
-                xtype: 'textfield',
-                fieldLabel: 'Login',
-                name: 'login',
-                allowBlank: false
-            }, {
-                xtype: 'textfield',
-                fieldLabel: 'Password',
-                name: 'password',
-                inputType: 'password',
-                allowBlank: false
-            }],
-            buttons: [{
-                text: 'Submit',
-                handler: function () {
-                    const form = authForm.getForm();
-                    if (form.isValid()) {
-                        authWindow.hide();
-                        showGridWindow();
-                    } else {
-                        Ext.Msg.alert('Ошибка', 'Заполните все обязательные поля!');
-                    }
-                }
-            }]
-        });
-
-        const authWindow = Ext.create('Ext.window.Window', {
+    createAuthWindow() {
+        const authForm = this.createAuthForm();
+        return Ext.create('Ext.window.Window', {
             title: 'Авторизация',
             closable: false,
             modal: true,
             items: [authForm]
         });
+    }
 
-        const showGridWindow = () => {
-            const gridWindow = Ext.create('Ext.window.Window', {
-                title: 'Список записей',
-                width: 500,
-                height: 300,
-                modal: true,
-                layout: 'fit',
-                items: [{
-                    xtype: 'grid',
-                    store: dataStore,
-                    selModel: {
-                        type: 'checkboxmodel'
-                    },
-                    columns: [{
-                        text: 'Name',
-                        dataIndex: 'name',
-                        flex: 1
-                    }]
-                }],
-                buttons: [{
+    createAuthForm() {
+        return Ext.create('Ext.form.Panel', {
+            width: 300,
+            bodyPadding: 10,
+            items: [
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Login',
+                    name: 'login',
+                    allowBlank: false
+                },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Password',
+                    name: 'password',
+                    inputType: 'password',
+                    allowBlank: false
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Submit',
+                    handler: () => this.handleAuthSubmit()
+                }
+            ]
+        });
+    }
+
+    handleAuthSubmit() {
+        const form = this.authWindow.down('form').getForm();
+        if (form.isValid()) {
+            this.authWindow.hide();
+            this.showGridWindow();
+        } else {
+            Ext.Msg.alert('Ошибка', 'Заполните все обязательные поля!');
+        }
+    }
+
+    showGridWindow() {
+        const gridWindow = Ext.create('Ext.window.Window', {
+            title: 'Список записей',
+            width: 500,
+            height: 300,
+            modal: true,
+            layout: 'fit',
+            items: [this.createGrid()],
+            buttons: [
+                {
                     text: 'OK',
-                    handler: function () {
-                        const selectedRecords = gridWindow.down('grid').getSelection();
-                        showViewport(selectedRecords);
-                        gridWindow.close();
-                    }
-                }, {
+                    handler: () => this.handleGridOk(gridWindow)
+                },
+                {
                     text: 'Cancel',
-                    handler: function () {
-                        gridWindow.close();
-                        authWindow.show();
-                    }
-                }]
-            });
+                    handler: () => this.handleGridCancel(gridWindow)
+                }
+            ]
+        });
 
-            gridWindow.show();
-        };
+        gridWindow.show();
+    }
 
-        const showViewport = (selectedRecords) => {
-            const selectedData = selectedRecords.map(record => record.data);
+    createGrid() {
+        return Ext.create('Ext.grid.Panel', {
+            store: this.dataStore,
+            selModel: {
+                type: 'checkboxmodel'
+            },
+            columns: [
+                {
+                    text: 'Name',
+                    dataIndex: 'name',
+                    flex: 1
+                }
+            ]
+        });
+    }
 
-            const viewportStore = Ext.create('Ext.data.Store', {
-                fields: ['name', 'checked'],
-                data: selectedData
-            });
+    handleGridOk(gridWindow) {
+        const grid = gridWindow.down('grid');
+        const selectedRecords = grid.getSelection();
+        this.showViewport(selectedRecords);
+        gridWindow.close();
+    }
 
-            Ext.create('Ext.container.Viewport', {
-                layout: 'vbox',
-                items: [{
+    handleGridCancel(gridWindow) {
+        gridWindow.close();
+        this.authWindow.show();
+    }
+
+    showViewport(selectedRecords) {
+        const selectedData = selectedRecords.map(record => record.data);
+
+        const viewportStore = Ext.create('Ext.data.Store', {
+            fields: ['name', 'checked'],
+            data: selectedData
+        });
+
+        Ext.create('Ext.container.Viewport', {
+            layout: 'vbox',
+            items: [
+                {
                     xtype: 'textfield',
                     fieldLabel: 'Login',
                     readOnly: true,
-                    value: authForm.getForm().findField('login').getValue()
-                }, {
+                    value: this.authWindow.down('form').getForm().findField('login').getValue()
+                },
+                {
                     xtype: 'textfield',
                     fieldLabel: 'Password',
                     inputType: 'password',
                     readOnly: true,
-                    value: authForm.getForm().findField('password').getValue()
-                }, {
+                    value: this.authWindow.down('form').getForm().findField('password').getValue()
+                },
+                {
                     xtype: 'grid',
                     title: 'Выбранные записи',
                     store: viewportStore,
-                    columns: [{
-                        text: 'Name',
-                        dataIndex: 'name',
-                        flex: 1
-                    }],
+                    columns: [
+                        {
+                            text: 'Name',
+                            dataIndex: 'name',
+                            flex: 1
+                        }
+                    ],
                     flex: 1,
                     width: '100%'
-                }]
-            });
-        };
-
-        authWindow.show();
+                }
+            ]
+        });
     }
-});
+}
